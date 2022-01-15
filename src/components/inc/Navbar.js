42a90ad6c0141559f8c84c0c4f99c43a93f298c7
Solange Mendes <solangejmendes@gmail.com>
2022-01-15 09:03:30 +0100

    buildvs9

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -1,19 +1,16 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
-import logo from '../images/rotor.png';
+import logo from '../images/logo.jpeg';
 
 export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white shadow">
-                <div className="container-fluid">
                     <NavLink to="/" className="navbar-brand">
-                        <img className="rotor" src={ logo } alt="logo" width="10%" />
+                        <img className="rotor" src={ logo } alt="logo" width="5%" />
                         ACE - Aire Comprimido Eficiente  
                     </NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                <div className="container-fluid">
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
